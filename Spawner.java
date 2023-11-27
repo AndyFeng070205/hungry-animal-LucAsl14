04c02ef932535b4c946ad4aa0a7de8fac9ea8063
LucAsl14 <lucas.fu070807@gmail.com>
2023-11-27 09:01:43 -0500

    Added options to tweak enemy spawn rate in code

--- Spawner.java
+++ Spawner.java
@@ -8,6 +8,13 @@ import java.util.*;
  */
 public class Spawner extends Actor
 {
+    int[] chances = {0, 30, 70};
+    int[] prefixSum = new int[chances.length+1];
+    public Spawner(){
+        for(int i=1; i<chances.length; i++){
+            prefixSum[i] = chances[i]+prefixSum[i-1];
+        }
+    }
     /**
      * Act - do whatever the Spawner wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -21,24 +28,30 @@ public class Spawner extends Actor
         int side = rand.nextInt(1, 5);
         int h = world.getHeight();
         int w = world.getWidth();
-        int roll = rand.nextInt(3);
         Enemy snake = new Enemy();
         Enemy2 archer = new Enemy2();
+        int roll = rand.nextInt(0,100);
+        Actor[] choices = {null, snake, archer};
+        Actor toAdd = snake;
+        for(int i=1; i<chances.length; i++){
+            if(roll<=prefixSum[i]) toAdd = choices[i];
+            if(roll>prefixSum[i]) break;
+        }
         switch(side){
             case 1:{
-                world.addObject(snake, 0, rand.nextInt(0, h-1));
+                world.addObject(toAdd, 0, rand.nextInt(0, h-1));
                 break;
             }
             case 2:{
-                world.addObject(snake, rand.nextInt(0, w-1), 0);
+                world.addObject(toAdd, rand.nextInt(0, w-1), 0);
                 break;
             }
             case 3:{
-                world.addObject(snake, w-1, rand.nextInt(0, h-1));
+                world.addObject(toAdd, w-1, rand.nextInt(0, h-1));
                 break;
             }
             case 4:{
-                world.addObject(snake, rand.nextInt(0, w-1), h-1);
+                world.addObject(toAdd, rand.nextInt(0, w-1), h-1);
                 break;
             }
         }
